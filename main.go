61d9dbceb2861d9a55f06b61829bafa380cf8e0a
enrico donelli <enrico.donelli@gmail.com>
2018-08-17 18:44:16 +0200

    lettura prime info

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"log"
@@ -21,18 +22,33 @@ func fujiXtransRead(inputFile *os.File) {
 
 }
 
+func readFromFileBytes(f *os.File, start int64, howmany int64) []byte {
+	o2, err := f.Seek(start, 0)
+	check(err)
+	thumbOffsetBytes := make([]byte, howmany)
+	n2, err := f.Read(thumbOffsetBytes)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n2, o2, string(thumbOffsetBytes))
+	return thumbOffsetBytes
+}
+
 // identify identifies the file maker
 func identify(inputFile *os.File) inputImage {
 
 	result := inputImage{make: "UNDEF"}
 
 	head := make([]byte, 32)
-	n1, err := inputFile.Read(head)
+	_, err := inputFile.Read(head)
 	check(err)
-	fmt.Printf("%d bytes : %s\n", n1, string(head))
+	//fmt.Printf("%d bytes : %s\n", n1, string(head))
 
 	if strings.HasPrefix(string(head), "FUJIFILM") {
 		result.make = "FUJIFILM"
+
+		thumbOffset := binary.BigEndian.Uint32(readFromFileBytes(inputFile, 84, 4))
+		thumbLength := binary.BigEndian.Uint32(readFromFileBytes(inputFile, 88, 4))
+		fmt.Println(thumbOffset)
+		fmt.Println(thumbLength)
 	}
 
 	return result
@@ -45,7 +61,7 @@ func check(e error) {
 }
 
 func main() {
-	rawfile := flag.String("f", "", "raw file")
+	rawfile := flag.String("f", "DSCF2483.RAF", "raw file")
 	flag.Parse()
 	log.Println("reading file " + *rawfile)
 
