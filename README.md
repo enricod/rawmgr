f3b3679853e2969b3ea2ad604642ab026cfb3a20
enricod <enrico.donelli@gmail.com>
2018-08-18 16:47:36 +0200

    identify fatto

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 nothing, yet
+
+Lettura RAF
+
+main 
+
+
+
+    if (filters || colors == 1)
+
+        imposta raw_image
+
+        entra in if (raw_image) #13111
+
+        fuji_rotate()
+        convert_to_rgb()
+        stretch()
+        
+
+    status = identify()
+        calcola thumb_offset
+        calcole thaumb_position
+        parse_tiff (data_offset = get4());
+        parse_tiff (thumb_offset+12);
+        apply_tiff();
+        parse_fuji( 4 bytest)
+            cerca num entries
+            per ogni entry
+                tag
+                len
+                tag == 0x131
+                    trova filters
+                    popola xtrans_abs
+
+                tag == 0x4949
+                    assegna order
+
+            
+            
+
--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"encoding/binary"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"strings"
 )
 
-type inputImage struct {
+type imageInfo struct {
 	make string
 }
 
@@ -18,25 +17,121 @@ const RawmgrVersion = "0.1"
 func xtransInterpolate() {
 
 }
+
+// getUint16 a partire da offset legge un int uint16 e torna la nuova posizione
+func getUint16(f *os.File, offset int64) (uint16, int64) {
+	value := binary.BigEndian.Uint16(readFromFileBytes(f, offset, 2))
+	return value, offset + 2
+}
+
+func get1Byte(f *os.File, offset int64) (uint16, int64) {
+	mybyte := readFromFileBytes(f, offset, 1)[0]
+	return uint16(mybyte), offset + 1
+}
+
+func readFromFile(f *os.File, offset int64, howmany int64) (uint32, int64) {
+	value := binary.BigEndian.Uint32(readFromFileBytes(f, offset, howmany))
+	return value, offset + howmany
+}
+func parseFuji(f *os.File, offset int64) {
+
+	var tag, len uint16
+	var start, posizione int64
+	var rawHeight, rawWidth uint16
+	var height, fujiLayout uint16
+	//var width uint32
+	//var fujiWidth uint32
+	var filters int
+	var xtransAbs [6][6]uint16
+
+	start = offset
+	entries, start := readFromFile(f, start, 4)
+
+	log.Printf("entries %d \n", entries)
+	if entries < 255 {
+
+		for i := 0; i < int(entries); i++ {
+			/*
+				tag = get2();
+				len = get2();
+				save = ftell(ifp);
+			*/
+			tag, start = getUint16(f, start)
+			len, start = getUint16(f, start)
+			posizione = start
+			log.Printf("posizione=%d, tag=%d, len=%d", posizione, tag, len)
+			switch tag {
+			case 0x100:
+				rawHeight, start = getUint16(f, start)
+				rawWidth, start = getUint16(f, start)
+				log.Printf("raw_width=%d, raw_height=%d", rawHeight, rawWidth)
+
+			case 0x121:
+				height, start = getUint16(f, start)
+				log.Printf("height=%d", height)
+
+			case 0x130:
+				fujiLayout, start = getUint16(f, start)
+				fujiLayout = fujiLayout >> 7
+				log.Printf("fujiLayout=%d", fujiLayout)
+			//fujiWidth = !(fgetc(ifp) & 8)
+			case 0x131:
+				filters = 9
+				var val uint16
+				for r := 5; r >= 0; r-- {
+					for c := 5; c >= 0; c-- {
+						val, start = get1Byte(f, start)
+						xtransAbs[r][c] = val & 3
+					}
+				}
+				log.Printf("filters=%d, xtransAbs=%v", filters, xtransAbs)
+				/*
+					filters = 9;
+					FORC(36) xtrans_abs[0][35-c] = fgetc(ifp) & 3;
+				*/
+
+			case 0x2ff0:
+				log.Printf("WARN tag non ancora elaborato=%d", tag)
+			case 0xc000:
+				if len > 20000 {
+					/*
+						c = order;
+						order = 0x4949;
+						while ((tag = get4()) > raw_width);
+						width = tag;
+						height = get4();
+						order = c;
+					*/
+
+				}
+			default:
+
+			}
+
+			start = posizione + int64(len)
+		}
+
+	}
+
+}
+
 func fujiXtransRead(inputFile *os.File) {
 
 }
 
 func readFromFileBytes(f *os.File, start int64, howmany int64) []byte {
-	o2, err := f.Seek(start, 0)
+	_, err := f.Seek(start, 0)
 	check(err)
-	thumbOffsetBytes := make([]byte, howmany)
-	n2, err := f.Read(thumbOffsetBytes)
+	retBytes := make([]byte, howmany)
+	f.Read(retBytes)
 	check(err)
-	fmt.Printf("%d bytes @ %d: %s\n", n2, o2, string(thumbOffsetBytes))
-	return thumbOffsetBytes
+	//log.Printf("lettura di %d bytes @ %d: %s\n", n2, o2, string(retBytes))
+	return retBytes
 }
 
 // identify identifies the file maker
-func identify(inputFile *os.File) inputImage {
-
-	result := inputImage{make: "UNDEF"}
-
+func identify(inputFile *os.File) imageInfo {
+	result := imageInfo{make: "UNDEF"}
 	head := make([]byte, 32)
 	_, err := inputFile.Read(head)
 	check(err)
@@ -44,16 +139,17 @@ func identify(inputFile *os.File) inputImage {
 
 	if strings.HasPrefix(string(head), "FUJIFILM") {
 		result.make = "FUJIFILM"
-
 		thumbOffset := binary.BigEndian.Uint32(readFromFileBytes(inputFile, 84, 4))
 		thumbLength := binary.BigEndian.Uint32(readFromFileBytes(inputFile, 88, 4))
-		fmt.Println(thumbOffset)
-		fmt.Println(thumbLength)
+		log.Printf("thumbOffset=%d, thumbLength=%d\n", thumbOffset, thumbLength)
+
+		startParse, _ := readFromFile(inputFile, 92, 4)
+		parseFuji(inputFile, int64(startParse))
 	}
 
 	return result
-
 }
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -68,8 +164,8 @@ func main() {
 	inputFile, err := os.Open(*rawfile)
 	check(err)
 
-	inputImage := identify(inputFile)
+	imageInfo := identify(inputFile)
 
-	fmt.Printf("Make: %s\n", inputImage.make)
+	log.Printf("Make: %s\n", imageInfo.make)
 	//
 }
