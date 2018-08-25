0ab6abea631c0615edaf0d7c23a8d64389ec6e5f
enricod <enrico.donelli@gmail.com>
2018-08-25 18:11:51 +0200

    migliorie lettura IFD

--- README.md
+++ README.md
@@ -1,39 +1,9 @@
-nothing, yet
 
-Lettura RAF
+# CR2
 
-main 
+Reads canon CR2 files
 
-
-
-    if (filters || colors == 1)
-
-        imposta raw_image
-
-        entra in if (raw_image) #13111
-
-        fuji_rotate()
-        convert_to_rgb()
-        stretch()
-        
-
-    status = identify()
-        calcola thumb_offset
-        calcole thaumb_position
-        parse_tiff (data_offset = get4());
-        parse_tiff (thumb_offset+12);
-        apply_tiff();
-        parse_fuji( 4 bytest)
-            cerca num entries
-            per ogni entry
-                tag
-                len
-                tag == 0x131
-                    trova filters
-                    popola xtrans_abs
-
-                tag == 0x4949
-                    assegna order
+written in go 
 
             
             
--- canon/canon.go
+++ canon/canon.go
@@ -1,6 +1,7 @@
 package canon
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"log"
@@ -8,7 +9,7 @@ import (
 	"github.com/enricod/rawmgr/common"
 )
 
-var Tags = map[uint16]string{
+var Tags = []map[uint16]string{{
 	0x0100: "width",
 	0x0101: "height",
 	272:    "model",
@@ -16,6 +17,15 @@ var Tags = map[uint16]string{
 	0x0112: "orientation",
 	0x011a: "xResolution",
 	0x8729: "exif",
+},
+	{
+		0x829a: "exposureTime",
+		0x829d: "fNumber",
+	},
+	{
+		0x0001: "canonCameraSettings",
+		0x0002: "canonFocalLength",
+	},
 }
 
 func check(e error) {
@@ -35,11 +45,12 @@ type Header struct {
 }
 
 type IFD struct {
-	Tag        uint16
-	Typ        uint16
-	Count      uint32
-	Value      uint32
-	NextOffset int64
+	Tag     uint16
+	Typ     uint16
+	Count   uint32
+	Value   uint32
+	Level   int
+	SubIFDs IFDs
 }
 
 type IFDs struct {
@@ -88,7 +99,7 @@ func readIfd(data []byte) IFD {
 	return result
 }
 
-func loopIfds(data []byte, order uint16, offset int64) IFDs {
+func loopIfds(data []byte, order uint16, offset int64, level int) IFDs {
 	ifdLength := int64(12)
 
 	var result IFDs
@@ -99,11 +110,20 @@ func loopIfds(data []byte, order uint16, offset int64) IFDs {
 	entries, start := common.ReadUint16Order(data, order, offset)
 	result.EntriesNr = entries
 
-	// log.Printf("Num. entries IFD=%d", entries)
 	for i := 0; i < int(entries); i++ {
 		ifdbytes := data[int(start):int(start+ifdLength)]
 		ifd := readIfd(ifdbytes)
-		items = append(items, ifd)
+		ifd.Level = level
+
+		switch ifd.Tag {
+		case 0x8769:
+			//log.Printf("trovata EXIF subdirectory")
+			ifd.SubIFDs = loopIfds(data, order, int64(ifd.Value), level+1)
+
+		case 0x927c:
+			log.Printf("trovata maker notes")
+			ifd.SubIFDs = loopIfds(data, order, int64(ifd.Value), level+1)
+		}
 		/*
 			if v, ok := Tags[ifd.Tag]; ok {
 				//do something here
@@ -112,6 +132,7 @@ func loopIfds(data []byte, order uint16, offset int64) IFDs {
 				log.Printf("Entry #%v", ifd)
 			}
 		*/
+		items = append(items, ifd)
 		start = start + ifdLength
 	}
 
@@ -129,7 +150,7 @@ func readIfds(data []byte, header *Header) []IFDs {
 	var nextIfdOffset = header.IfdOffset
 
 	for nextIfdOffset > 0 {
-		ifds = loopIfds(data, header.ByteOrder, nextIfdOffset)
+		ifds = loopIfds(data, header.ByteOrder, nextIfdOffset, 0)
 		result = append(result, ifds)
 		nextIfdOffset = ifds.NextIfdOffset
 		//log.Printf("ifds:%v, nextOffset=%d", ifds, nextIfdOffset)
@@ -137,12 +158,44 @@ func readIfds(data []byte, header *Header) []IFDs {
 	return result
 }
 
+func nSpaces(spaces int) string {
+	var buffer bytes.Buffer
+
+	for i := 0; i < spaces; i++ {
+		buffer.WriteString("    ")
+	}
+	return buffer.String()
+
+}
+func dumpIfd(ifd IFD) {
+	var desc string
+	if v, ok := Tags[ifd.Level][ifd.Tag]; ok {
+		desc = v
+	} else {
+		desc = "Tag "
+	}
+	log.Printf("%s %s #%v, Value=%v, Count=%d", nSpaces(ifd.Level), desc, ifd.Tag, ifd.Value, ifd.Count)
+	for j := 0; j < len(ifd.SubIFDs.Ifds); j++ {
+		ifd2 := ifd.SubIFDs.Ifds[j]
+		dumpIfd(ifd2)
+	}
+}
+func dumpIfds(ifds []IFDs) {
+	for i := 0; i < len(ifds); i++ {
+		log.Printf("IFD #%d\n", i)
+		ifdrow := ifds[i]
+		for k := 0; k < len(ifdrow.Ifds); k++ {
+			ifd := ifdrow.Ifds[k]
+			dumpIfd(ifd)
+
+		}
+	}
+}
 func Process(data []byte) {
 	canonHeader, err := readHeader(data)
 	check(err)
 	log.Printf("Header %v\n", canonHeader)
 	ifds := readIfds(data, &canonHeader)
-	for i := 0; i < len(ifds); i++ {
-		log.Printf("%v\n\n\n", ifds[i])
-	}
+
+	dumpIfds(ifds)
 }
