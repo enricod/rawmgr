f74803c5b31cd5f92846d000849d3f12f8af1936
enrico donelli <enrico.donelli@gmail.com>
2018-08-29 12:35:12 +0200

    inizio parsing huff

--- canon/canon.go
+++ canon/canon.go
@@ -267,9 +267,60 @@ func saveJpeg(data []byte, aifd IFDs, filename string, calc calcStartEnd) {
 
 }
 
+/*
+ushort * CLASS make_decoder_ref (const uchar **source)
+{
+    int max, len, h, i, j;
+    const uchar *count;
+    ushort *huff;
+
+    count = (*source += 16) - 17;
+    for (max=16; max && !count[max]; max--);
+    huff = (ushort *) calloc (1 + (1 << max), sizeof *huff);
+    merror (huff, "make_decoder()");
+    huff[0] = max;
+    for (h=len=1; len <= max; len++)
+        for (i=0; i < count[len]; i++, ++*source)
+            for (j=0; j < 1 << (max-len); j++)
+                if (h <= 1 << max)
+                    huff[h++] = len << 8 | **source;
+    return huff;
+}
+*/
+/*
+   Construct a decode tree according the specification in *source.
+   The first 16 bytes specify how many codes should be 1-bit, 2-bit
+   3-bit, etc.  Bytes after that are the leaf values.
+
+   For example, if the source is
+
+    { 0,1,4,2,3,1,2,0,0,0,0,0,0,0,0,0,
+      0x04,0x03,0x05,0x06,0x02,0x07,0x01,0x08,0x09,0x00,0x0a,0x0b,0xff  },
+
+   then the code is
+
+	00		0x04
+	010		0x03
+	011		0x05
+	100		0x06
+	101		0x02
+	1100		0x07
+	1101		0x01
+	11100		0x08
+	11101		0x09
+	11110		0x00
+	111110		0x0a
+	1111110		0x0b
+	1111111		0xff
+*/
+func decodeHuffTree(data []byte) {
+	log.Printf("huff data %v", data)
+}
+
 func parseDHTHeader(data []byte, offset int64) (DHTHeader, error) {
 	var dhtHeader = DHTHeader{}
 
+	log.Printf("parseDHTHeader, offset=%d\n", offset)
 	marker, offset2 := common.ReadUint16(data, offset)
 	if marker != 0xffc4 {
 		return dhtHeader, fmt.Errorf("DHT Marker not valid  %d", marker)
@@ -280,23 +331,8 @@ func parseDHTHeader(data []byte, offset int64) (DHTHeader, error) {
 	length, offset2 := common.ReadUint16(data, offset2)
 	dhtHeader.Length = length
 
-	tableClass := uint8(data[offset2])
-	dhtHeader.TableClass0 = tableClass
-	offset2++
-
-	tableIndex := uint8(data[offset2])
-	dhtHeader.TableIndex0 = tableIndex
-	offset2++
-
-	offset2 = offset2 + int64(length)
-	tableClass1 := uint8(data[offset2])
-	dhtHeader.TableClass1 = tableClass1
-	offset2++
-
-	tableIndex1 := uint8(data[offset2])
-	dhtHeader.TableIndex1 = tableIndex1
-	offset2++
-
+	huffBytes := data[offset2 : offset2+int64(length)]
+	decodeHuffTree(huffBytes)
 	return dhtHeader, nil
 }
 
