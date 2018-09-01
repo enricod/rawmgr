8461f72b8d6b2d180052cd134ca70292d5f1e9d7
enricod <enrico.donelli@gmail.com>
2018-09-01 16:22:17 +0200

    download test images

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -O "images/Canon-EOS6D-RAW.CR2" https://raw.pixls.us/getfile.php/799/nice/Canon%20-%20EOS%206D%20-%20RAW.CR2 
+
--- images/download.py
+++
@@ -1,9 +0,0 @@
-
-import os.path
-
-baseUrl = "https://raw.pixls.us/getfile.php"
-cameras = ["/799/nice/Canon%20-%20EOS%206D%20-%20RAW.CR2", "/1680/nice/Fujifilm%20-%20X-Pro2%20-%2014bit%20uncompressed%20\(3:2\).RAF", "/2156/nice/Sony%20-%20DSC-RX100M3%20-%2012bit%20compressed%20\(3:2\).ARW"]
-for c in cameras:
-    f = c.split("/")[-1]
-    bashCommand = "wget " + baseUrl + c
-    os.system(bashCommand)
